a040f5740295ffe3030f72d0a872336975d047a4
Karol Horosin <karol.horosin@gmail.com>
2018-08-26 00:28:08 +0200

    Better user exception handling and getting user info

--- Dawn/src/main/java/com/horosin/dawn/repository/UserRepository.java
+++ Dawn/src/main/java/com/horosin/dawn/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.horosin.dawn.repository;
 
 import com.horosin.dawn.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository("userRepository")
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
+    Optional<User> findById(Long sampleId);
+
 }
